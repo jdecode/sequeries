15e4da3f92dc5807a3efc99e67d6bc4a3598b7ff
JD Singh <jagdeepsingh@founderandlightning.com>
2022-12-31 00:30:17 +0530

    Add shield as the favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title> {{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/x-icon"/>
         <link rel="stylesheet" href="{{ asset('fonts/material-icons.css') }}" />
         <link rel="stylesheet" href="{{ asset('fonts/octicons.css') }}" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
