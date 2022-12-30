dd7efd288218a04176376895ad173a0b92c66cf5
JD Singh <jagdeepsingh@founderandlightning.com>
2022-12-31 00:51:20 +0530

    Use svg as logo-image + Add name

--- resources/views/components/admin/desktop-sidebar.blade.php
+++ resources/views/components/admin/desktop-sidebar.blade.php
@@ -5,9 +5,14 @@
     xmlns:x-bind="http://www.w3.org/1999/xhtml">
     <!-- Sidebar component, swap this element with another sidebar if you like -->
     <div class="flex flex-col flex-grow pt-5 bg-gray-300 dark:bg-gray-800 overflow-y-auto">
-        <div class="flex items-center flex-shrink-0 px-1">
-            <a href="{{ route('admin.dashboard') }}">
+        <div class="flex items-center flex-shrink-0 px-4">
+            <a href="{{ route('admin.dashboard') }}" class="flex items-center gap-3 text-dev-500 text-2xl">
                 <x-application-logo class="block h-10 w-auto fill-current"></x-application-logo>
+                <span
+                    x-cloak x-show="!sidebarCollapsed"
+                    class="">
+                    Sequeries
+                </span>
             </a>
         </div>
         <div class="mt-5 flex-1 flex flex-col">
--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,5 @@
 <span
     title=" {{ config('app.name', 'Laravel') }}"
-    class="text-dev-500 material-icons-outlined text-7xl">shield</span>
+    class="text-dev-500 text-7xl">
+    <img src="{{ asset('favicon.svg') }}" alt="logo"/>
+</span>
